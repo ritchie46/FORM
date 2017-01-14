4280c0e45b70e2a8a927ea5a31b9196537b50de0
ritchie46 <ritchie46@gmail.com>
2017-01-14 15:34:26 +0100

    parser fixes

--- FORM/parser.py
+++ FORM/parser.py
@@ -19,7 +19,7 @@ class Parser:
         self.f = parse_expr(self.f)
 
     def _create_symbols(self):
-        sml = re.findall(r'\b[a-z]\b', self.f)
+        sml = re.findall(r'\b[a-zA-Z]\b', self.f)
 
         for s in sml:
             if s not in self.variables.keys():
@@ -36,19 +36,7 @@ class Parser:
         for i in range(9):
             self.f = self.f.replace(old[i], new[i])
 
-        # The power sign need to be inserted between the letters and the numbers and insert **
-        s1 = re.split(r"[a-zA-Z0-9]\d", self.f)
-        s2 = re.findall(r'[a-zA-Z0-9]\d', self.f)
-
-        expr = ""
-        count = 0
-        for st in s1:
-            expr += st
-
-            if count < len(s2):
-                expr += s2[count][0] + "**" + s2[count][1]
-            count += 1
-        self.f = expr
+        # # The power sign need to be inserted between the letters and the numbers and insert **
         self.f = self.f.replace('^', '**')
 
 
--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ if sys.version_info[0] < 3:
     sys.exit('Sorry, Python 2.x is not supported')
 
 setup(
-    name='FORM',
-    version=0.1,
+    name='FORM-cal',
+    version="0.2.1",
     description='Taylor series approximation of the performance function of different stochastic variables.',
     author='Ritchie Vink',
     author_email='ritchie46@gmail.com',
-    url='www.ritchievink.com',
+    url='http://ritchievink.com',
     license='MIT License',
     packages=['FORM'],
     install_requires=[
